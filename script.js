90bb78932b909c6533dbf28279470643222ff8ad
GN-load <71946339+GN-load@users.noreply.github.com>
2020-11-08 14:08:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// const users = [{name: 'Mango', active: true}, {name: 'Poly', active: false}, {name: 'Ajax', active: true}, {name: 'Lux', active: false}];
+// const toggle = async (all, name, callback) => {
+//     const update = all.map(user => 
+//         user.name === name ? {...user, active: !user.active} : user,
+//         );
+
+//         return await update
+// };
+
+// const logger = update => console.table(update);
+
+// const a = 'Dima'
+
+// localStorage.setItem('name', a);
+// alert(localStorage.getItem('name'));
+
+console.log('hello')
